27c4d6c8d45b4f4caefb42663a65c06075fe8410
Simon Phillips <simonxphillips@duck.com>
2023-10-05 16:16:40 -0400

    Test

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from '@/components/Header';
 import '../../styles/styles.scss';
 import '@material/web/button/outlined-button.js';
+import '@material/web/fab/fab.js';
+import '@material/web/icon/icon.js';
 
 const TestPage: React.FC = () => {
   return (
@@ -11,6 +13,9 @@ const TestPage: React.FC = () => {
       <h1 className='section-header'>Test Page for new components</h1>
       <p>This page is unlisted as it is used for testing UI components I am thinking of using.</p>
       <md-outlined-button>Back</md-outlined-button>
+      <md-fab aria-label="Edit">
+  <md-icon slot="icon">edit</md-icon>
+</md-fab>
     </div>
   );
 };
