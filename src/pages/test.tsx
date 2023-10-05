7d8dfe1af03839258acaac72cadfed6072c38084
Simon Phillips <simonxphillips@duck.com>
2023-10-05 15:52:23 -0400

    Test of MWC

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Header from '@/components/Header';
 import '../../styles/styles.scss';
+import '@material/web/button/outlined-button.js';
 
 const TestPage: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const TestPage: React.FC = () => {
       <Header/>
       <h1 className='section-header'>Test Page for new components</h1>
       <p>This page is unlisted as it is used for testing UI components I am thinking of using.</p>
+      <md-outlined-button>Back</md-outlined-button>
     </div>
   );
 };
