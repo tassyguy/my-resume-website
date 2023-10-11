cc8cda9b42f88cea58e76dd9170c2407a18f6198
Simon Phillips <simonxphillips@duck.com>
2023-10-11 17:42:02 -0400

    Added branded header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,18 @@
 // src/components/Header.tsx
 import React from 'react';
 import Link from 'next/link';
-// import Image from 'next/image';
+import Image from 'next/image';
 
 const Header: React.FC = () => {
   return (
     <header className="navbar">
       <div className="logo">
-        
         <Link href="/">
-          {/*<Image src="/man.svg" alt="My logo" width={24} height={24} className="logoImage"/> */}🧔🏻‍♂️
+          <Image src="/images/logo-dark.jpg" alt="My logo" width={48} height={48} className="logoImage"/>
         </Link>
       </div>
       <nav className="nav-items">
         <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
           <li>
             <Link href="/about">About</Link>
           </li>
